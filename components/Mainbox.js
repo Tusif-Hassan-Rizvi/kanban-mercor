8a94a4a369e3e0212de203ca303586a311409da1
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2023-06-18 12:26:19 +0530

    task box added

--- components/Mainbox.js
+++ components/Mainbox.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from "../src/styles/mainbox.module.css";
 import Navbar from './Navbar'
 import Maintopbar from './Maintopbar'
+import Mainbottombar from './Mainbottombar'
 
 function Mainbox({getExpandNavbar, isexpand}) {
   return (
@@ -11,6 +12,7 @@ function Mainbox({getExpandNavbar, isexpand}) {
       {/* mainbox top section  */}
       <section className={`${styles.main_box}`}>
       <Maintopbar></Maintopbar>
+      <Mainbottombar></Mainbottombar>
       </section>
     </div>
   )
